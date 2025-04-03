92d513388829c908c02cf812be838bb3a20ea6b9
HenriqueSSan <hsstudentwork@gmail.com>
2025-04-03 01:28:02 -0300

    feat: add header in application

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div>
+	<header>
+		<nav class="mx-auto flex max-w-[1280px] items-center justify-between py-4">
+			<figure>
+				<img width="256" src="/logo.svg" alt="" />
+			</figure>
+
+			<div class="flex items-center gap-4 bg-amber-100 px-3 py-2 rounded-xl">
+				<a class="flex items-center gap-2" href="/">
+					<img width="24" height="24" src="https://cdn.countryflags.com/thumbs/united-states-of-america/flag-round-250.png" alt="" />
+					<span class="text-amber-500">En</span>
+                </a>
+
+                <a class="flex items-center gap-2" href="/">
+					<img width="24" height="24" src="https://cdn.countryflags.com/thumbs/brazil/flag-round-250.png" alt="" />
+					<span class="text-amber-500">Pt-br</span>
+                </a>
+			</div>
+		</nav>
+	</header>
+
+    
+</div>
