7e3e821a750c77a732d5aae0ceaa80c89a648b2f
HenriqueSSan <hsstudentwork@gmail.com>
2025-04-24 13:03:23 -0300

    feat: add initial screen

---
+++ src/core/modules/initial/screen/initial-screen.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import AboutSection from '../components/about-section.svelte';
+	import BenefitSection from '../components/benefit-section.svelte';
+	import HealthSambaSection from '../components/health-samba-section.svelte';
+	import HeroSection from '../components/hero-section.svelte';
+	import TestimonialSection from '../components/testimonial-section.svelte';
+</script>
+
+<svelte:head>
+	<title>Luana Michelly - Samba Cura - A Nova Rainha do Carnaval</title>
+</svelte:head>
+
+<main>
+	<HeroSection />
+	<AboutSection />
+	<BenefitSection />
+	<HealthSambaSection />
+	<TestimonialSection />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,5 @@
-<div>
-	<header>
-		<nav class="mx-auto flex max-w-[1280px] items-center justify-between py-4">
-			<figure>
-				<img width="256" src="/logo.svg" alt="" />
-			</figure>
+<script>
+	import InitialScreen from '../core/modules/initial/screen/initial-screen.svelte';
+</script>
 
-			<div class="flex items-center gap-4 bg-amber-100 px-3 py-2 rounded-xl">
-				<a class="flex items-center gap-2" href="/">
-					<img width="24" height="24" src="https://cdn.countryflags.com/thumbs/united-states-of-america/flag-round-250.png" alt="" />
-					<span class="text-amber-500">En</span>
-                </a>
-
-                <a class="flex items-center gap-2" href="/">
-					<img width="24" height="24" src="https://cdn.countryflags.com/thumbs/brazil/flag-round-250.png" alt="" />
-					<span class="text-amber-500">Pt-br</span>
-                </a>
-			</div>
-		</nav>
-	</header>
-
-    
-</div>
+<InitialScreen />
