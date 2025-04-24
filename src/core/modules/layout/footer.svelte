30063096bd42a94f7d9f28c6fc6086ef6285c1bb
HenriqueSSan <hsstudentwork@gmail.com>
2025-04-24 13:01:48 -0300

    feat: add footer in page

---
+++ src/core/modules/layout/footer.svelte
@@ -0,0 +1,12 @@
+<footer class="bg-amber-400 py-[36px] text-black">
+	<div class="mx-auto flex max-w-[1280px] justify-between">
+		<p>© Copyright 2025 Luana Michelly. All rights reserved.</p>
+		<p>
+			Developed by <a
+				target="_blank"
+				href="https://lutruvials.vercel.app"
+				class="font-bold underline">Lutruvials</a
+			>
+		</p>
+	</div>
+</footer>
