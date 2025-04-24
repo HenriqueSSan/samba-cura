1e367c19ecab759c89c7e632f1ae4cf0568aed09
HenriqueSSan <hsstudentwork@gmail.com>
2025-04-24 13:02:05 -0300

    feat: add about section

---
+++ src/core/modules/initial/components/about-section.svelte
@@ -0,0 +1,27 @@
+<section class="bg-black py-[96px] text-white">
+    <div
+        class="mx-auto flex max-w-[1280px] flex-col items-center justify-between gap-10 px-4 lg:flex-row lg:px-0"
+    >
+        <div class="space-y-4 lg:w-1/2">
+            <h2 class="text-3xl font-bold lg:text-5xl">Veja a história da nossa rainha.</h2>
+            <p class="text-md font-medium lg:text-xl">
+                Comecei no Carnaval ainda menina,tudo começou em 1990, temos uma história sambistica, Sou
+                uma sambista apaixonada e Sonhadora que vive o Samba.
+            </p>
+            <p class="text-md font-medium lg:text-xl">
+                Com a minha arte trabalho com shows de Carnaval , sambashows e eventos , e descobri que o
+                dom de ensinar me encantou, e levo sempre para as minhas alunas que todas nós somos Rainha
+                e Musas, e falo sempre que o samba cura.
+            </p>
+        </div>
+
+        <div class="lg:h-[664px] grow columns-3 gap-1 lg:w-1/4">
+            <img class="my-1 lg:min-h-[320px]" src="/col-1.png" height="392" width="252" alt="" />
+            <img class="my-1 lg:max-h-[192px]" src="/col-b-1.png" height="248" width="248" alt="" />
+            <img class="my-1 lg:max-h-[192px]" src="/col-2.png" height="248" width="248" alt="" />
+            <img class="my-1 lg:min-h-[320px]" src="/col-3.png" height="392" width="252" alt="" />
+            <img class="my-1 lg:min-h-[320px]" src="/col-b-2.png" height="392" width="252" alt="" />
+            <img class="my-1 lg:max-h-[192px]" src="/col-b-3.png" height="248" width="248" alt="" />
+        </div>
+    </div>
+</section>
