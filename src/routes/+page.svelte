9939c7f8bc2052fbb70bcf00c0124832c163e8ff
HenriqueSSan <hsstudentwork@gmail.com>
2025-05-15 07:05:00 -0300

    feat: add meta tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,14 @@
 	import InitialScreen from '../core/modules/initial/screen/initial-screen.svelte';
 </script>
 
+<svelte:head>
+	<meta
+		name="description"
+		content="“Vem Sambar, O Samba Cura!” com Luana Michelly sambista desde 1990, é uma professora a mais de 8 anos na escola de samba, com seu método de ensino e com sua íncrivel frase: “Todas nós somos Rainha e Musas”"
+	/>
+	<meta name="keywords" content="Samba, Vem Sambar, Samba Cura, Cura" />
+	<meta name="author" content="Luana Michelli">
+	<meta name="creator" content="https://lutruvials.com">
+</svelte:head>
+
 <InitialScreen />
