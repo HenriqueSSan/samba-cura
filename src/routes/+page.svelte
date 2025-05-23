a395682435b196f4c24fe8cd05332c43953f52e1
HenriqueSSan <hsstudentwork@gmail.com>
2025-05-23 06:52:37 -0300

    fix: og and twitter banner url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,22 +11,22 @@
 	/>
 
 	<meta property="og:type" content="website" />
-	<meta property="og:url" content="https://samba-cura.com/" />
+	<meta property="og:url" content="https://www.samba-cura.com/" />
 	<meta property="og:title" content="Samba Cura - Vem Sambar, O Samba Cura!" />
 	<meta
 		property="og:description"
 		content="“Vem Sambar, O Samba Cura!” com Luana Michelly sambista desde 1990, é uma professora a mais de 8 anos na escola de samba, com seu método de ensino e com sua íncrivel frase: “Todas nós somos Rainha e Musas”"
 	/>
-	<meta property="og:image" content="/banner.png" />
+	<meta property="og:image" content="https://www.samba-cura.com/banner.png" />
 
 	<meta property="twitter:card" content="summary_large_image" />
-	<meta property="twitter:url" content="https://samba-cura.com/" />
+	<meta property="twitter:url" content="https://www.samba-cura.com/" />
 	<meta property="twitter:title" content="Samba Cura - Vem Sambar, O Samba Cura!" />
 	<meta
 		property="twitter:description"
 		content="“Vem Sambar, O Samba Cura!” com Luana Michelly sambista desde 1990, é uma professora a mais de 8 anos na escola de samba, com seu método de ensino e com sua íncrivel frase: “Todas nós somos Rainha e Musas”"
 	/>
-	<meta property="twitter:image" content="/banner.png" />
+	<meta property="twitter:image" content="https://www.samba-cura.com/banner.png" />
 
 	<meta name="author" content="Luana Michelli" />
 	<meta name="creator" content="https://lutruvials.com" />
